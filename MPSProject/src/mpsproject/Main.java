a98835370004ca6256419d594c6e3ba19c793a46
chanta <sfsdf@sfd.com>
2013-10-28 17:46:23 +0200

    Citire fisier

--- MPSProject/src/mpsproject/Main.java
+++ MPSProject/src/mpsproject/Main.java
@@ -5,17 +5,40 @@
 
 package mpsproject;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
 /**
  *
  * @author Administrator
  */
-public class Main {
+public class Main
+{
+    public static void read(String name, int dim) throws FileNotFoundException
+    {
+        Scanner scanner = new Scanner(new File(name));
+        dim =  scanner.nextInt();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        int [][] mat = new int[dim][dim];
+
+        for (int i = 0; i < dim; i++)
+           for(int j = 0 ; j < dim; j++)
+           {
+                if(scanner.hasNextInt())
+                {
+                   mat[i][j] = scanner.nextInt();
+                }
+           }
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException
+    {
+
+
+
     }
 
 }
